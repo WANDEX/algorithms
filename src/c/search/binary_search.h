da71c983edc025403985941acb63f9b3832504c0
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-03 13:18:15 +0300

    split binary_search on header & unit tests

--- src/c/search/binary_search.h
+++ src/c/search/binary_search.h
@@ -1,11 +1,5 @@
 #include <stddef.h> // size_t
 
-#define COUT 0 // NOTE: toggle printing of stdout messages ON/OFF
-
-#if COUT
-#include <stdio.h>
-#endif
-
 /**
  * find the index of value in sorted array! O(log(n))
  */
@@ -19,34 +13,4 @@ int binary_search(const int *array, const size_t n, const int value)
         else if (array[mid] > value) high = mid - 1;
     }
     return -1; // Value not found
-}
-
-int test(const int *array, const int value, const int eindex)
-{
-    if (binary_search(array, sizeof(array), value) == eindex)
-        return 1;
-    return 0;
-}
-
-int tests()
-{
-    int pass = 0, fail = 0;
-    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
-
-    test(array, 3,  2) ? ++pass : ++fail;
-    test(array, 8, -1) ? ++pass : ++fail;
-    test(array, 9,  7) ? ++pass : ++fail;
-
-#if COUT
-    if (!fail)
-        printf("[binary_search.c] ALL PASSED: %d\n", pass);
-    else
-        printf("[PASS]: %d\t[FAIL]: %d\t[TNUM]: %d\n", pass, fail, pass + fail);
-#endif
-    return fail; // 0 -> all tests are passed, else num of failed tests
-}
-
-int main(void)
-{
-    return tests();
 }
---
+++ tst/unit/search/binary_search.cc
@@ -0,0 +1,28 @@
+#include "binary_search.h"
+#include "gtest/gtest.h"
+
+
+int binary_search_test(const int *array, const int value, const int eindex)
+{
+    if (binary_search(array, sizeof(array), value) == eindex)
+        return 1;
+    return 0;
+}
+
+TEST(binary_search_test, 1)
+{
+    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
+    EXPECT_TRUE(binary_search_test(array, 3, 2));
+}
+
+TEST(binary_search_test, 2)
+{
+    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
+    EXPECT_TRUE(binary_search_test(array, 8, -1));
+}
+
+TEST(binary_search_test, 3)
+{
+    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
+    EXPECT_TRUE(binary_search_test(array, 9, 7));
+}
