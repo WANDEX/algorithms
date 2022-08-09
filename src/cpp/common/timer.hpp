d3e34494f534568d4ddc172063dbba06100154ea
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-09 23:50:20 +0300

    [timer.hpp]

---
+++ src/cpp/common/timer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+
+class Timer
+{
+private:
+    // Type aliases to make accessing nested type easier
+    using clock_type = std::chrono::steady_clock;
+    using second_type = std::chrono::duration<double, std::ratio<1>>;
+
+    std::chrono::time_point<clock_type> m_beg { clock_type::now() };
+
+public:
+    inline void reset()
+    {
+        m_beg = clock_type::now();
+    }
+
+    inline double elapsed() const
+    {
+        return std::chrono::duration_cast<second_type>(clock_type::now() - m_beg).count();
+    }
+};
