d3a48620b66f8dad93072e916bf86d4c03701156
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-18 13:36:04 +0300

    [print.hpp] +print std::vector, support other value types

--- src/cpp/common/print.hpp
+++ src/cpp/common/print.hpp
@@ -1,29 +1,43 @@
 #pragma once
 
+#include <array>
 #include <cstddef>  // std::size_t
-#include <array>    // std::array
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
 /**
  * print C array
  */
-inline void print_array(const int A[], const std::size_t n, const std::string prefix="")
+inline void print_array(const auto a[], const std::size_t n, const std::string prefix="")
 {
     std::cout << prefix << "{";
     for (std::size_t i = 0; i < n - 1; i++)
-        std::cout << A[i] << ", ";
-    std::cout << A[n - 1] << "}" << std::endl; // last element without additional ', '
+        std::cout << a[i] << ", ";
+    std::cout << a[n - 1] << "}" << std::endl; // last element without additional ', '
 }
 
 /**
  * print std::array
  */
-template <const std::size_t n>
-inline void print_array(const std::array<int, n> &A, const std::string prefix="")
+template <typename T, const std::size_t n>
+inline void print_array(const std::array<T, n> &a, const std::string prefix="")
 {
     std::cout << prefix << "{";
     for (std::size_t i = 0; i < n - 1; i++)
-        std::cout << A[i] << ", ";
-    std::cout << A[n - 1] << "}" << std::endl; // last element without additional ', '
+        std::cout << a[i] << ", ";
+    std::cout << a[n - 1] << "}" << std::endl; // last element without additional ', '
+}
+
+/**
+ * print std::vector
+ */
+template <typename T>
+inline void print_array(const std::vector<T> &a, const std::string prefix="")
+{
+    const std::size_t n = a.size();
+    std::cout << prefix << "{";
+    for (std::size_t i = 0; i < n - 1; i++)
+        std::cout << a[i] << ", ";
+    std::cout << a[n - 1] << "}" << std::endl; // last element without additional ', '
 }
