16ecd182fdfc752d13c47827bceb434affa46de6
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-19 13:09:41 +0300

    [macro.hpp]

---
+++ src/cpp/common/macro.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * is sizeof type/object <= 2 addresses of memory.
+ * -> cheap to copy (only if has no setup costs!). Cost of common types:
+ * cheap: all fundamental types, enumerated types, std::string_view.
+ * expns: std::array, std::string, std::vector, std::ostream, most of the classes.
+ */
+#define isSmall(T) (sizeof(T) <= 2 * sizeof(void*))
