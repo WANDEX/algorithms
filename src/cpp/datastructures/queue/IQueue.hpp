9daa6741c384d010f9b24e1a6abc052695e46774
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-24 13:53:04 +0300

    [IQueue.hpp] interface of Queue data structure

---
+++ src/cpp/datastructures/queue/IQueue.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstddef>      // size_t
+
+// Interface of the Queue data structure
+template<typename T>
+class IQueue
+{
+public:
+    // return the number of elements in the queue
+    virtual std::size_t size() const = 0;
+
+    // return if the queue is empty
+    virtual bool isEmpty() const = 0;
+
+    // enqueue the element to the back of the queue
+    virtual void enqueue(const T &elem) = 0;
+
+    // dequeue the element from the front of the queue
+    virtual T dequeue() = 0;
+
+    // peek the element at the front of the queue
+    virtual T peek() const = 0;
+
+    // virtual destructor in case we delete an IQueue pointer,
+    // so the proper derived destructor is called
+    virtual ~IQueue() {}
+};
