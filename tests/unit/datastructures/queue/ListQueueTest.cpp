bf413bb88a7d9e81a1f9282cbf172b5abf07eda1
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-08-24 13:55:38 +0300

    [ListQueueTest.cpp] tests of the ListQueue class

---
+++ tests/unit/datastructures/queue/ListQueueTest.cpp
@@ -0,0 +1,101 @@
+#include "ListQueue.hpp"
+
+#include <gtest/gtest.h>
+
+#include <stdexcept>        // runtime_error
+#include <string>
+
+class ListQueueTest : public ::testing::Test {
+protected:
+    ListQueue<int> *queue;
+
+    ListQueueTest()
+    {
+        queue = new ListQueue<int>();
+    }
+
+    ListQueueTest(const int &elem)
+    {
+        queue = new ListQueue<int>(elem);
+    }
+
+    virtual ~ListQueueTest()
+    {
+        if (queue) delete queue;
+    }
+
+    virtual void SetUp()
+    {}
+
+    virtual void TearDown()
+    {
+        queue->clear();
+    }
+
+};
+
+TEST_F(ListQueueTest, testEmptyQueue)
+{
+    EXPECT_TRUE(queue->isEmpty());
+    EXPECT_EQ(queue->size(), 0);
+}
+
+TEST_F(ListQueueTest, testDequeueOnEmpty)
+{
+    try {
+        queue->dequeue();
+    } catch(std::runtime_error const &err) {
+        EXPECT_EQ(err.what(), std::string("Empty Queue"));
+    } catch(...) {
+        FAIL() << "Expected std::runtime_error Empty Queue";
+    }
+}
+
+TEST_F(ListQueueTest, testPeekOnEmpty)
+{
+    try {
+        queue->peek();
+    } catch(std::runtime_error const &err) {
+        EXPECT_EQ(err.what(), std::string("Empty Queue"));
+    } catch(...) {
+        FAIL() << "Expected std::runtime_error Empty Queue";
+    }
+}
+
+TEST_F(ListQueueTest, testEnqueue)
+{
+    queue->enqueue(2);
+    EXPECT_EQ(queue->size(), 1);
+}
+
+TEST_F(ListQueueTest, testPeek)
+{
+    queue->enqueue(2);
+    EXPECT_EQ(2, queue->peek());
+    EXPECT_EQ(queue->size(), 1);
+}
+
+TEST_F(ListQueueTest, testDequeue)
+{
+    queue->enqueue(2);
+    EXPECT_EQ(2, queue->dequeue());
+    EXPECT_EQ(queue->size(), 0);
+}
+
+TEST_F(ListQueueTest, testExhaustively)
+{
+    ASSERT_TRUE(queue->isEmpty());
+    queue->enqueue(1);
+    ASSERT_FALSE(queue->isEmpty());
+    queue->enqueue(2);
+    EXPECT_EQ(queue->size(), 2);
+    EXPECT_EQ(1, (int) queue->peek());
+    EXPECT_EQ(queue->size(), 2);
+    EXPECT_EQ(1, (int) queue->dequeue());
+    EXPECT_EQ(queue->size(), 1);
+    EXPECT_EQ(2, (int) queue->peek());
+    EXPECT_EQ(queue->size(), 1);
+    EXPECT_EQ(2, (int) queue->dequeue());
+    EXPECT_EQ(queue->size(), 0);
+    ASSERT_TRUE(queue->isEmpty());
+}
