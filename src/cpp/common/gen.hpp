100d8f5420939f646b1bf0ca4342849fd0535333
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-09-10 17:46:12 +0300

    [gen.hpp] if unique=true vector will contain only unique elements.

--- src/cpp/common/gen.hpp
+++ src/cpp/common/gen.hpp
@@ -68,18 +68,25 @@ inline std::array<T, n> random(const double fr, const double to)
 /**
  * generate random std::vector.
  * n: size, limits: fr ... to
+ * if unique=true vector will contain only unique elements.
  */
 template<typename T>
-inline std::vector<T> random(const std::size_t n, const double fr, const double to)
+inline std::vector<T> random(const std::size_t n, const double fr, const double to, const bool unique=false)
 {
     if (n < 1) return {};
+    if (unique) {
+        std::unordered_set<T> uset = gen::random_uset<T>(n, fr, to);
+        std::vector<T> out { uset.begin(), uset.end() };
+        return std::move(out);
+    }
+
     std::vector<T> out;
     out.reserve(n);
     std::random_device rd;
     std::default_random_engine rng(rd());
     std::uniform_real_distribution<> dist(fr, to);
     for (std::size_t i = 0; i < n; i++)
-        out.push_back(dist(rng));
+        out[i] = dist(rng);
     return std::move(out);
 }
 
