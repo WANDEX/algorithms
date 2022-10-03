dd168b638e7a5bcfa79d3d0449b028d0bc5b839a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-10-03 17:20:55 +0300

    [BSTuptrNode.hpp] BST node class which uses std::unique_ptr for nodes

---
+++ src/cpp/datastructures/binarysearchtree/BSTuptrNode.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>           // std::unique_ptr
+
+template <typename T>
+class Node
+{
+public:
+    using node_ptr = std::unique_ptr<Node<T>>;
+    T m_data;
+    node_ptr l, r; // left/right node
+
+    Node(const T& data, node_ptr& left, node_ptr& right)
+        : m_data{ data }, l{ left }, r{ right }
+    {}
+
+    Node(const T& data)
+        : m_data{ data }
+    {}
+
+    Node() = default;
+    Node(Node &&) = default;
+    Node(const Node &) = default;
+    Node &operator=(Node &&) = default;
+    Node &operator=(const Node &) = default;
+    virtual ~Node() = default;
+};
