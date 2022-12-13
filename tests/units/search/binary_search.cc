9789af5f44c4dad3862fa37955b27b0ffcb2fdc4
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-12-13 13:32:22 +0300

    [binary_search.cc] improved tests, coverage: branch 100%, line 100%

--- tests/units/search/binary_search.cc
+++ tests/units/search/binary_search.cc
@@ -1,27 +1,53 @@
-#include <gtest/gtest.h>
 #include "binary_search.h"
 
-int binary_search_test(const int *array, const int value, const int eindex)
+#include <gtest/gtest.h>
+
+#include <stddef.h>             // size_t
+
+TEST(binary_search_test, ZeroSizeArray)
 {
-    if (binary_search(array, sizeof(array), value) == eindex)
-        return 1;
-    return 0;
+    const int array[1] = {1};
+    // Zero size arrays are an extension (but still, just in case)
+    // here we supply fake 0 size (in order to have full branch coverage)
+    ASSERT_DEATH(binary_search(array, 0, 0), "n != 0");
+    ASSERT_DEATH(binary_search(array, 0, 1), "n != 0");
 }
 
-TEST(binary_search_test, 1)
+TEST(binary_search_test, simple)
 {
-    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
-    EXPECT_TRUE(binary_search_test(array, 3, 2));
+    const size_t n = 1;
+    const size_t NF = n + 1; // Not Found
+    const int array[n] = {0};
+    EXPECT_EQ(binary_search(array, n, 0), 0);
+    EXPECT_EQ(binary_search(array, n, 1), NF);
 }
 
-TEST(binary_search_test, 2)
+TEST(binary_search_test, OutOfBounds)
 {
-    const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
-    EXPECT_TRUE(binary_search_test(array, 8, -1));
+    const size_t n = 1;
+    const size_t NF = n + 1; // Not Found
+    const int array[n] = {1};
+    ASSERT_EQ(binary_search(array, n, 0), NF);
+    ASSERT_EQ(binary_search(array, n, 1), 0);
 }
 
-TEST(binary_search_test, 3)
+TEST(binary_search_test, testExaustive)
 {
     const int array[] = {1, 2, 3, 4, 5, 6, 7, 9, 16, 17};
-    EXPECT_TRUE(binary_search_test(array, 9, 7));
+    const size_t n = sizeof(array) / sizeof(array[0]);
+    const size_t NF = n + 1; // Not Found
+    ASSERT_EQ(binary_search(array, n, 0), NF);
+    EXPECT_EQ(binary_search(array, n, 1), 0);
+    EXPECT_EQ(binary_search(array, n, 2), 1);
+    EXPECT_EQ(binary_search(array, n, 3), 2);
+    EXPECT_EQ(binary_search(array, n, 4), 3);
+    EXPECT_EQ(binary_search(array, n, 5), 4);
+    EXPECT_EQ(binary_search(array, n, 6), 5);
+    EXPECT_EQ(binary_search(array, n, 7), 6);
+    EXPECT_EQ(binary_search(array, n, 8), NF);
+    EXPECT_EQ(binary_search(array, n, 9), 7);
+    EXPECT_EQ(binary_search(array, n, 16), 8);
+    EXPECT_EQ(binary_search(array, n, 17), 9);
+    EXPECT_EQ(binary_search(array, n, 18), NF);
 }
+
