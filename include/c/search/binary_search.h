5e0e92eff7d6f5a2ea51c8c8f5d9c0c9d4181f3f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-12-13 11:58:10 +0300

    [binary_search.h] overhaul, fixes, doc, return n + 1 if not found

--- include/c/search/binary_search.h
+++ include/c/search/binary_search.h
@@ -1,18 +1,31 @@
 #pragma once
 
-#include <stddef.h> // size_t
+#include <assert.h>
+#include <stddef.h>             // size_t
 
 /**
- * find the index of value in sorted array! O(log(n))
+ * @brief find the index of value in sorted array! O(log(n))
+ *
+ * @param  sorted_array
+ * @param  n - number of elements in array
+ * @param  needle - find a needle in a haystack
+ * @return n + 1 if not found
+ * @return index of needle
  */
-int binary_search(const int *array, const size_t n, const int value)
+size_t binary_search(const int *sorted_array, const size_t n, const int needle)
 {
-    size_t high = n - 1, mid = 0, low = 0;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (array[mid] == value) return mid;
-        else if (array[mid] < value) low  = mid + 1;
-        else if (array[mid] > value) high = mid - 1;
+    // Zero size arrays are an extension (but still, just in case)
+    assert(n != 0); // XXX LCOV_EXCL_LINE (what is & why: hidden 0 branch?)
+    size_t hi = n, lo = 0;
+    size_t mid = (lo + hi) / 2;
+    while (mid <= n && lo <= hi) {
+        if (sorted_array[mid] == needle)
+            return mid;
+        if (sorted_array[mid] <  needle)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+        mid = (lo + hi) / 2;
     }
-    return -1; // Value not found
+    return n + 1; // Not Found
 }
