dfc402cf5c737959047da97a0f9b2c07d4fb6b16
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-12-13 14:54:58 +0300

    [IStack.hpp] fix: function coverage

--- include/cpp/datastructures/stack/IStack.hpp
+++ include/cpp/datastructures/stack/IStack.hpp
@@ -12,16 +12,19 @@ class IStack
 public:
     // return the number of elements in the stack
     virtual std::size_t size() const = 0;
+
     // return if the stack is empty
     virtual bool isEmpty() const = 0;
+
     // push the element on the stack
     virtual void push(const T &elem) = 0;
+
     // pop the element off the stack
     virtual T pop() = 0;
+
+    // peek at the top of the stack
     virtual T peek() const = 0;
-    // virtual destructor in case we delete an IStack pointer,
-    // so the proper derived destructor is called
-    virtual ~IStack() {}
+
 };
 
 } // namespace ds
