1949772215df0742c9ef450e79a53c2c00d3af5c
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-12-14 14:39:44 +0300

    [gcov.sh] use fd if found at PATH, or else use find + xargs

--- scripts/gcov.sh
+++ scripts/gcov.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+## gcov wrapper - support any gcov argument as script options
 
 set -e
+
+at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+
+if ! at_path gcov; then
+  echo "gcov not found at PATH! Exit."
+  exit 1
+fi
 
 # gcov flags to enable colors via ascii escape sequences
 TERM_COLORS=1
@@ -23,27 +31,18 @@ fi
 
 incl_dir=$(realpath "./include")
 ccov_dir=$(realpath "./.coverage")
-gcov_dir="${ccov_dir}/gcov"
-lcov_dir="${ccov_dir}/lcov"
-lcov_info="${lcov_dir}/lcov.info"
 
+gcov_dir="${ccov_dir}/gcov"
 [ -d "$gcov_dir" ] && rm -rf "$gcov_dir"
-[ -d "$lcov_dir" ] && rm -rf "$lcov_dir"
 mkdir -p "$gcov_dir"
-mkdir -p "$lcov_dir"
 cd "$gcov_dir" || exit 12
 
-# covp=$(find "$bdir" -name '*.gcda' -print0 | xargs -0 gcov | tail -n 1 | awk '{sub(/executed:/,""); printf "%d", $2}')
-
 # shellcheck disable=SC2068 # Intentional - to re-split trailing arguments.
-# NOTE: pass -b to see branch info
-fd . "$bdir" -tf -e gcda -x gcov -s "$incl_dir" "$flags" $@
+# NOTE: pass -b as argument of the script to see branch info
+if at_path fd; then
+  fd . "$bdir" -tf -e gcda -x gcov -s "$incl_dir" "$flags" $@
 
-# extra empty line
-echo ""
-
-lcov --directory "$bdir" --capture --output-file "$lcov_info" -rc lcov_branch_coverage=1
-# remove output for external libraries (i.e. gtest)
-lcov --remove "$lcov_info" "/usr*" -o "$lcov_info"
-genhtml "$lcov_info" --branch-coverage --output-directory "$lcov_dir"
+elif at_path find && at_path xargs; then
+  find "$bdir" -name '*.gcda' -print0 | xargs -0 gcov -s "$incl_dir" "$flags" $@
+fi
 
