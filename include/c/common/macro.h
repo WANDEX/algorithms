5bac9e54e8bc87e900edfe646338ec33db17f56e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-12-29 12:30:00 +0300

    [macro.h] added clarifying notes on prohibiting the use of macro
    renamed: include/cpp/common/macro.hpp -> include/c/common/macro.h

---
+++ include/c/common/macro.h
@@ -0,0 +1,17 @@
+#ifndef WNDX_MACRO_H
+#define WNDX_MACRO_H
+
+// NOTE:
+// Rule of thumb - macro should not be used in any of C++ related files.
+// To not pollute the global namespace of the C++ project code.
+// NESTING INTO C++ NAMESPACE WILL NOT WORK! (i.e. namespace ... { }) =>
+// Macros are handled before the compiler ever sees the code,
+// and preprocessor knows nothing about C++ namespaces.
+
+// WNDX_IS_SMALL - sizeof type/object <= 2 addresses of memory.
+// -> cheap to copy (only if has no setup costs!). Cost of common types:
+// cheap: all fundamental types, enumerated types, std::string_view.
+// expns: std::array, std::string, std::vector, std::ostream, most of the classes.
+#define WNDX_IS_SMALL(T) (sizeof(T) <= 2 * sizeof(void*))
+
+#endif // WNDX_MACRO_H
