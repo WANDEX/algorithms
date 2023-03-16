8885c4f9be0b9365562435ccae38e3a1fab5ce94
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-03-16 23:17:30 +0300

    [cmake] ## comments, to differentiate easily from commented out lines

--- cmake/funcs.cmake
+++ cmake/funcs.cmake
@@ -1,21 +1,21 @@
-# useful functions / variable definitions
+## useful functions / variable definitions
 
-# define variables to be able to use short if conditions
-# if(${NOT_Release})
+## define variables to be able to use short if conditions
+## if(${NOT_Release})
 function(not_build_type ARG)
-    if(NOT CMAKE_BUILD_TYPE STREQUAL ${ARG})
+  if(NOT CMAKE_BUILD_TYPE STREQUAL ${ARG})
     set(NOT_${ARG} TRUE PARENT_SCOPE)
   else()
     set(NOT_${ARG} FALSE PARENT_SCOPE)
   endif()
 endfunction(not_build_type)
-# creation of variables to be able to use them later
+## creation of variables to be able to use them later
 not_build_type(Release)
 not_build_type(Debug)
 
 
-# define variables to be able to use short & readable if conditions
-# if(${Clang_COMP} OR ${GNU_COMP})
+## define variables to be able to use short & readable if conditions
+## if(${Clang_COMP} OR ${GNU_COMP})
 function(under_compiler ARG)
   if( (  CMAKE_C_COMPILER_ID STREQUAL ${ARG}) OR
       (CMAKE_CXX_COMPILER_ID STREQUAL ${ARG}) )
@@ -27,7 +27,7 @@ function(under_compiler ARG)
     set(${ARG}_COMP FALSE PARENT_SCOPE)
   endif()
 endfunction(under_compiler)
-# creation of variables to be able to use them later
+## creation of variables to be able to use them later
 under_compiler(GNU)
 under_compiler(Clang)
 under_compiler(AppleClang)
@@ -36,25 +36,25 @@ under_compiler(cl)    # XXX: MSVC cl compiler (assuming this is somehow possible
                       # to compile under linux -> (without big hassle with VStudio & wine)
 
 
-# recursively include all sub-directories of the given dir
+## recursively include all sub-directories of the given dir
 function(include_subdirs arg_dir)
   file(GLOB_RECURSE recurse_rpaths LIST_DIRECTORIES true
     RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${arg_dir}/$")
-  # ^ list only sub-directories of the given dir using relative path
+  ## ^ list only sub-directories of the given dir using relative path
   list(FILTER recurse_rpaths EXCLUDE REGEX "/\\.") # exclude paths with .dirs
   foreach(rel_path ${recurse_rpaths})
-    # get absolute path (relative paths have problems particularly in Windows)
+    ## get absolute path (relative paths have problems particularly in Windows)
     file(REAL_PATH "${rel_path}" abs_path BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
-    # double check that this is directory
+    ## double check that this is directory
     if(NOT IS_DIRECTORY "${abs_path}")
       if(${NOT_Release})
         message("(SKIP) THIS IS NOT DIRECTORY: ${abs_path}")
       endif()
       continue()
     endif()
-    # include
+    ## include dir
     include_directories("${abs_path}")
-    # see which dirs are included in the build process
+    ## see which dirs are included in the build process
     if(${NOT_Release})
       message("included subdir: ${abs_path}")
     endif()
--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,13 +1,13 @@
-# For Windows: Prevent overriding the parent project's compiler/linker settings
+## For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-# prefer GoogleTest version installed in the system || fetch if not found
+## prefer GoogleTest version installed in the system || fetch if not found
 find_package(GTest 1.12.1)
 if(NOT GTest_FOUND)
   include(FetchContent)
   FetchContent_Declare(
     GoogleTest # release-1.12.1
     URL https://github.com/google/googletest/archive/58d77fa8070e8cec2dc1ed015d66b454c8d78850.zip
-    )
+  )
   FetchContent_MakeAvailable(GoogleTest)
 endif()
 enable_testing()
