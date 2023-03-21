19e19f0b884b043bbc8d4ac3ca4b52629fcb2145
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-03-21 23:27:01 +0300

    [merge_sort.hpp] added docs, renamed variables for readability

--- include/wndx/sort/merge_sort.hpp
+++ include/wndx/sort/merge_sort.hpp
@@ -1,6 +1,7 @@
 #pragma once
-// merge sort algorithm without VLA (Variable Length Arrays).
-// VLA are a C99 feature and not supported by msvc.
+// merge sort algorithm using std::vector for the sub-arrays.
+// written without VLA (Variable Length Arrays) =>
+// VLA are a C99 feature and not supported by MSVC.
 
 #include <array>
 #include <cassert>
@@ -12,132 +13,174 @@ namespace srt {
 
 namespace /* (anonymous) */ {
 
+/**
+ * @brief merge of merge_sort - sorts sub-arrays, then merges them together.
+ *
+ * @param  a - input array to sort in place.
+ * @param lo - lower bound.
+ * @param  m - index middle.
+ * @param up - upper bound.
+ */
 template<typename T>
 inline void
-merge(T *A, const std::size_t p, const std::size_t q, const std::size_t r)
+merge(T *a, const std::size_t lo, const std::size_t m, const std::size_t up)
 {
     std::size_t x{0}, y{0}, z{0};
-    std::size_t const lsub{ q - p + 1 };
-    std::size_t const rsub{ r - q };
+    std::size_t const lsub{ m - lo + 1 };
+    std::size_t const rsub{ up - m };
 
     std::vector<T> la(lsub);
     std::vector<T> ra(rsub);
 
-    // copy separate fragments of the input array into two temporary arrays
-    for (x = 0; x < lsub; x++) la[x] = A[p + x];
-    for (y = 0; y < rsub; y++) ra[y] = A[q + y + 1];
+    // split input array on the halves
+    for (x = 0; x < lsub; x++) la[x] = a[lo + x];
+    for (y = 0; y < rsub; y++) ra[y] = a[m + y + 1];
 
-    x = 0; y = 0; z = p;
+    x = 0; y = 0; z = lo;
     while (x < lsub && y < rsub) {
         if (la[x] <= ra[y])
-            A[z++] = la[x++];
+            a[z++] = la[x++];
         else
-            A[z++] = ra[y++];
+            a[z++] = ra[y++];
     }
 
-    while (x < lsub) A[z++] = la[x++];
-    while (y < rsub) A[z++] = ra[y++];
+    while (x < lsub) a[z++] = la[x++];
+    while (y < rsub) a[z++] = ra[y++];
 }
 
 /**
- * @brief merge of merge sort algorithm.
- * sorts & merges the sorted sub-arrays.
- * (abbreviated as it is a well-known algorithm)
- * @p left_index, @q middle_index, @r right_index
+ * @brief merge of merge_sort - sorts sub-arrays, then merges them together.
+ *
+ * @param  a - input array to sort in place.
+ * @param lo - lower bound.
+ * @param  m - index middle.
+ * @param up - upper bound.
  */
 template<typename T, std::size_t n>
 inline void
-merge(std::array<T, n> &A, const std::size_t p, const std::size_t q, const std::size_t r)
+merge(std::array<T, n> &a, const std::size_t lo, const std::size_t m, const std::size_t up)
 {
     std::size_t x{0}, y{0}, z{0};
-    std::size_t const lsub{ q - p + 1 };
-    std::size_t const rsub{ r - q };
+    std::size_t const lsub{ m - lo + 1 };
+    std::size_t const rsub{ up - m };
 
     std::vector<T> la(lsub);
     std::vector<T> ra(rsub);
 
-    // copy separate fragments of the input array into two temporary arrays
-    for (x = 0; x < lsub; x++) la[x] = A[p + x];
-    for (y = 0; y < rsub; y++) ra[y] = A[q + y + 1];
+    // split input array on the halves
+    for (x = 0; x < lsub; x++) la[x] = a[lo + x];
+    for (y = 0; y < rsub; y++) ra[y] = a[m + y + 1];
 
-    x = 0; y = 0; z = p;
+    x = 0; y = 0; z = lo;
     while (x < lsub && y < rsub) {
         if (la[x] <= ra[y])
-            A[z++] = la[x++];
+            a[z++] = la[x++];
         else
-            A[z++] = ra[y++];
+            a[z++] = ra[y++];
     }
 
-    while (x < lsub) A[z++] = la[x++];
-    while (y < rsub) A[z++] = ra[y++];
+    while (x < lsub) a[z++] = la[x++];
+    while (y < rsub) a[z++] = ra[y++];
 }
 
+/**
+ * @brief merge of merge_sort - sorts sub-arrays, then merges them together.
+ *
+ * @param  a - input array to sort in place.
+ * @param lo - lower bound.
+ * @param  m - index middle.
+ * @param up - upper bound.
+ */
 template<typename T>
 inline void
-merge(std::vector<T> &A, const std::size_t p, const std::size_t q, const std::size_t r)
+merge(std::vector<T> &a, const std::size_t lo, const std::size_t m, const std::size_t up)
 {
     std::size_t x{0}, y{0}, z{0};
-    std::size_t const lsub{ q - p + 1 };
-    std::size_t const rsub{ r - q };
+    std::size_t const lsub{ m - lo + 1 };
+    std::size_t const rsub{ up - m };
 
     std::vector<T> la(lsub);
     std::vector<T> ra(rsub);
 
-    // copy separate fragments of the input array into two temporary arrays
-    for (x = 0; x < lsub; x++) la[x] = A[p + x];
-    for (y = 0; y < rsub; y++) ra[y] = A[q + y + 1];
+    // split input array on the halves
+    for (x = 0; x < lsub; x++) la[x] = a[lo + x];
+    for (y = 0; y < rsub; y++) ra[y] = a[m + y + 1];
 
-    x = 0; y = 0; z = p;
+    x = 0; y = 0; z = lo;
     while (x < lsub && y < rsub) {
         if (la[x] <= ra[y])
-            A[z++] = la[x++];
+            a[z++] = la[x++];
         else
-            A[z++] = ra[y++];
+            a[z++] = ra[y++];
     }
 
-    while (x < lsub) A[z++] = la[x++];
-    while (y < rsub) A[z++] = ra[y++];
+    while (x < lsub) a[z++] = la[x++];
+    while (y < rsub) a[z++] = ra[y++];
 }
 
 } // (anonymous) [internal_linkage]
 
+/**
+ * @brief merge sort algorithm.
+ *
+ * @param   a - input array to sort in place.
+ * @param  lo - lower bound.
+ * @param  up - upper bound.
+ *
+ * recursively calls itself to split array until it becomes a single element.
+ * sorts sub-arrays, then merges them together using merge function.
+ */
 template<typename T>
-inline void merge_sort(T *A, const std::size_t p, const std::size_t r)
+inline void merge_sort(T *a, const std::size_t lo, const std::size_t up)
 {
-    if (p >= r) return;
-    assert(A); // XXX
-    const std::size_t q{ (p + r) / 2 }; // middle index
-    merge_sort(A, p, q);
-    merge_sort(A, q + 1, r);
-    merge(A, p, q, r);
+    if (lo >= up) return;
+    assert(a); // XXX
+    const std::size_t m{ (lo + up) / 2 }; // middle index
+    merge_sort(a, lo, m);
+    merge_sort(a, m + 1, up);
+    merge(a, lo, m, up);
 }
 
 /**
  * @brief merge sort algorithm.
- * recursively calls itself to divide array until it becomes a single element,
- * then merges the sorted sub-arrays.
- * @p left_index, @r right_index
+ *
+ * @param   a - input array to sort in place.
+ * @param  lo - lower bound.
+ * @param  up - upper bound.
+ *
+ * recursively calls itself to split array until it becomes a single element.
+ * sorts sub-arrays, then merges them together using merge function.
  */
 template<typename T, std::size_t n>
-inline void merge_sort(std::array<T, n> &A, const std::size_t p, const std::size_t r)
+inline void merge_sort(std::array<T, n> &a, const std::size_t lo, const std::size_t up)
 {
-    if (p >= r) return;
+    if (lo >= up) return;
     static_assert(n != 0); // XXX
-    const std::size_t q{ (p + r) / 2 }; // middle index
-    merge_sort(A, p, q);
-    merge_sort(A, q + 1, r);
-    merge(A, p, q, r);
+    const std::size_t m{ (lo + up) / 2 }; // middle index
+    merge_sort(a, lo, m);
+    merge_sort(a, m + 1, up);
+    merge(a, lo, m, up);
 }
 
+/**
+ * @brief merge sort algorithm.
+ *
+ * @param   a - input array to sort in place.
+ * @param  lo - lower bound.
+ * @param  up - upper bound.
+ *
+ * recursively calls itself to split array until it becomes a single element.
+ * sorts sub-arrays, then merges them together using merge function.
+ */
 template<typename T>
-inline void merge_sort(std::vector<T> &A, const std::size_t p, const std::size_t r)
+inline void merge_sort(std::vector<T> &a, const std::size_t lo, const std::size_t up)
 {
-    if (p >= r) return;
-    assert(!A.empty()); // XXX
-    const std::size_t q{ (p + r) / 2 }; // middle index
-    merge_sort(A, p, q);
-    merge_sort(A, q + 1, r);
-    merge(A, p, q, r);
+    if (lo >= up) return;
+    assert(!a.empty()); // XXX
+    const std::size_t m{ (lo + up) / 2 }; // middle index
+    merge_sort(a, lo, m);
+    merge_sort(a, m + 1, up);
+    merge(a, lo, m, up);
 }
 
 } // namespace srt
