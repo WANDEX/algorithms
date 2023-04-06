25809da14f629fbfa0eabf8963fca1b838ac55cf
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-06 23:11:17 +0300

    [main.cc] fix: death_test_style threadsafe - for all unit tests

--- tests/units/main.cc
+++ tests/units/main.cc
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
+    GTEST_FLAG_SET(death_test_style, "threadsafe");
     return RUN_ALL_TESTS();
 }
 
