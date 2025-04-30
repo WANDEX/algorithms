94b25e2ed81d9055dc789e0f2ab034f5c130bc26
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-04-30 17:33:29 +0300

    [build.sh] upd - same as in sane

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-## build & optionally run tests.
+## build & optionally run tests. AUTHOR: WANDEX.
+## Script is meant only for the project developer and as the reference.
+## No guarantees that it will work on your OS/platform!
+## Use standard cmake commands for building the project.
+## Or adapt script steps yourself for your personal preference.
+##
 ## NOTE: manually export following environment variables
+## or pass -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++
 ## to build/test under multiple compilers before submitting code!
 #
 # export CC=gcc   CXX=g++
@@ -19,22 +25,69 @@
 ## or
 # ./scripts/build.sh gtest *wildcard*
 
-bt="Debug"
-compiler="${CC:-_}"
-# get compiler basename in case declared via full path
-cmbn=$(basename "$compiler")
-bdir="build/dev-$bt-$cmbn"
-
 set -e
-
-beg_epoch=$(date +%s) # for the first call of the time_spent_on_step
 
 if [ ! -r ./CMakeLists.txt ]; then
   printf "%s\n" "Current directory does not contain CMakeLists.txt, exit."
   exit 1
 fi
 
-[ -n "$CTEST_OUTPUT_ON_FAILURE" ] || export CTEST_OUTPUT_ON_FAILURE=1
+at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+
+notify() {
+  EC="$?" ## save error/exit code of the previously executed command
+  at_path notify-send || return 0
+  stag="string:x-dunst-stack-tag"
+  bg="string:bgcolor:"
+  fg="string:fgcolor:"
+  arg="$2"
+  case "$1" in
+    error|ERROR)     urg="critical" ;;
+    warning|WARNING) urg="normal" ;;
+    *)
+      urg="low"
+      bg="string:bgcolor:#11FF44"
+      fg="string:fgcolor:#000000"
+      arg="$1"
+    ;;
+  esac
+  notify-send -u "$urg" -h "$stag:$prj_name" -h "$stag:hi" -h "$bg" -h "$fg" "[$prj_name]" "$arg"
+}
+
+# shellcheck disable=SC2068 # Intentional - to re-split trailing arguments.
+run_ctest() { cmake -E time ctest --output-on-failure --test-dir "$_bdir" $@ ;} # shortcut
+
+sep="=============================================================================="
+vsep() { printf "\n%b%.78s%b\n\n" "${2}" "[${1}]${sep}" "${END}" ;}
+
+
+prj_name=""
+prj_name_upper=""
+if at_path rg; then
+  prj_name=$(rg -Ue '\bproject\([\s]*[\n]*\b(\w+)\b' --trim -or '$1' ./CMakeLists.txt)
+else
+  prj_name=$(basename "$(git rev-parse --show-toplevel)")
+fi
+prj_name_upper=$(echo "$prj_name" | tr "[:lower:]" "[:upper:]")
+
+
+PRJ_BUILD_TESTS="${BUILD_TESTS:-"${prj_name_upper}_BUILD_TESTS"}"
+bt="${BUILD_TYPE:-Debug}"
+generator="${GENERATOR:-Ninja}" # "MSYS Makefiles", "Unix Makefiles", Ninja
+compiler="${CC:-_}"
+verbose="${VERBOSE:-0}"
+deploy="${DEPLOY:-0}"
+rel="${REL:-0}"
+cmbn=$(basename "$compiler") # get compiler basename in case declared via full path
+bdir="build/dev-$bt-$cmbn"
+
+
+_verbose=""
+cmake_log_level=""
+if [ "$verbose" = 1 ]; then
+  _verbose="--verbose"
+  cmake_log_level="--log-level=DEBUG"
+fi
 
 opt="$1"
 test_filter="$2"
@@ -46,71 +99,77 @@ case "$opt" in
     test_filter="$3"
     fresh="--fresh"
     clean_first="--clean-first"
-    ;;
+  ;;
   cleaner|cc) # for edge cases like corrupted GCDA files etc.
     opt="$2"
     test_filter="$3"
     [ -d "$bdir" ] && rm -rf "$bdir"
     mkdir -p "$bdir"
-    ;;
+  ;;
 esac
-
-time_spent_on_step() {
-  # HACK: because standard utility/command 'time' does not work.
-  end_epoch=$(date +%s)
-  diff_secs=$((end_epoch-beg_epoch))
-  # small insignificant optimization
-  [ "$diff_secs" = 0 ] && beg_epoch="$end_epoch" && return
-  spent_time=$(date -d "@${diff_secs}" "+%Mm %Ss")
-  beg_epoch=$(date +%s)
-  printf "step took:%b %s.%b\n" "${BLD}" "$spent_time" "${END}"
-}
-
-und='=========================='
-sep="${und}${und}${und}"
-vsep() {
-  time_spent_on_step
-  printf "\n%s[%s]\n%s\n\n" "${2}" "${1}" "${sep}${END}"
-}
-
-vsep "CONFIGURE" "${BLU}"
-cmake -S . -B "$bdir" -G Ninja -D CMAKE_BUILD_TYPE=${bt} -Wdev -Werror=dev ${fresh}
-
-vsep "BUILD" "${CYN}"
-cmake --build "$bdir" --config ${bt} ${clean_first}
-
-gtest_binary="./$bdir/tests/units/tests_units"
-if [ ! -x "$gtest_binary" ]; then
-  printf "%s\n^ %s\n" "$gtest_binary" \
-    "File not found or not executable, exit."
-  exit 5
+## for toggling building of the tests
+if [ -n "$opt" ]; then
+  tt=ON
+else
+  tt=OFF
 fi
 
-# shellcheck disable=SC2068 # Intentional - to re-split trailing arguments.
-run_ctest() { ctest --test-dir "$bdir" $@ ;} # shortcut
+## source and build dirs provided as the relative paths
+if [ "$rel" = 1 ]; then
+  cd "$bdir" || exit 11
+  pwd
+  _sdir="../.."
+  _bdir="."
+else
+  _sdir="."
+  _bdir="$bdir"
+fi
+
+vsep "CONFIGURE" "${BLU}"
+cmake -E time \
+cmake -S "$_sdir" -B "$_bdir" -G "$generator" -D CMAKE_BUILD_TYPE="${bt}" -D "$PRJ_BUILD_TESTS=${tt}" \
+-Wdev -Werror=dev ${fresh} ${cmake_log_level} \
+|| { notify ERROR "CONFIGURE ERROR" ; exit "$EC" ;}
+
+vsep "BUILD" "${CYN}"
+cmake -E time \
+cmake --build "$_bdir" --config "${bt}" ${clean_first} ${_verbose} \
+|| { notify ERROR "BUILD ERROR" ; exit "$EC" ;}
 
 [ -n "$opt" ] && vsep "TESTS" "${RED}"
 [ -n "$test_filter" ] || test_filter='.*'
 case "$opt" in
   ctest|ct)
-    # optionally filter by regex
+    ## optionally filter by regex
     run_ctest -R "$test_filter"
-    ;;
+  ;;
   ctp)
-    # shortcut for terse progress output
+    ## shortcut for terse progress output
     run_ctest --progress -R "$test_filter"
-    ;;
+  ;;
   ctr)
-    # suppress output, errors, always return success (to not immediately exit because of 'set -e')
+    ## suppress output, errors, always return success (to not immediately exit because of 'set -e')
     run_ctest -R "$test_filter" >/dev/null 2>&1 || true
-    # rerun previously failed (gives live (filtered) results, with only failed tests)
+    ## rerun previously failed (gives live (filtered) results, with only failed tests)
     run_ctest --rerun-failed
-    ;;
+  ;;
   gtest|gt)
-    # optionally filter by wildcard ':'-separated patterns '*', '?' ('-' negative)
+    gtest_binary="./$_bdir/bin/tests_units"
+    if [ "$tt" = ON ] && [ ! -x "$gtest_binary" ]; then
+      printf "%s\n^ %s\n" "$gtest_binary" "File not found or not executable, exit."
+      exit 5
+    fi
+    ## optionally filter by wildcard ':'-separated patterns '*', '?' ('-' negative)
     [ "$test_filter" = '.*' ] && test_filter='*'
     "$gtest_binary" --gtest_filter="$test_filter"
-    ;;
+  ;;
 esac
-[ -n "$opt" ] && vsep "COMPLETED" "${GRN}"
+
+if [ "$deploy" = 1 ]; then
+  vsep "DEPLOY" "${MAG}"
+  cmake --install "$_bdir" --config "${bt}" --prefix "$_bdir/deploy"
+fi
+
+vsep   "COMPLETED" "${GRN}"
+notify "COMPLETED"
 
