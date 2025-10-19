080ae9c3f92fc7b8c53744561b2d83f31ee6113b
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-10-19 06:22:10 +0300

    [wndx_cmake_build.sh] printe - stderr, $CMAKE, check_prerequisites(), comments

--- scripts/wndx_cmake_build.sh
+++ scripts/wndx_cmake_build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-## build & optionally run tests. AUTHOR: WANDEX.
+## AUTHOR : github.com/WANDEX
+## LICENCE: MIT
+##
+## wndx cmake build script & optionally run tests. (POSIX sh compliant)
+##
 ## Script is meant only for the project developer and as the reference.
 ## No guarantees that it will work on your OS/platform!
 ## Use standard cmake commands for building the project.
@@ -17,28 +21,11 @@
 # export PATH0="$PATH" # backup unmodified PATH
 # export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.36.32532/bin/Hostx64/x64"
 # export PATH="$PATH:/c/wndx/src/top/DrMemory-Windows-2.6.0/bin64"
-# export PATH="$PATH:/c/wndx/src/top/DrMemory-Windows-2.6.0/bin"
 # rm -rf ./build/msvc/ && mkdir -p ./build/msvc/
 # cmake -S . -B ./build/msvc/ -G "Visual Studio 17 2022" -DWNDX_ALGO_BUILD_TESTS=ON -DWNDX_ALGO_MEMCHECK_ENABLE=ON
 # cmake --build ./build/msvc/
 # ctest --test-dir ./build/msvc/tests/units/
 # cmake --build ./build/msvc/ --target memcheck
-#
-#
-## build:
-# ./scripts/build.sh
-## build & run tests:
-# ./scripts/build.sh ctest
-## make clean build & run tests:
-# ./scripts/build.sh clean ctest
-## make cleaner build (for edge cases) & run tests:
-# ./scripts/build.sh cleaner ctest
-## build & run filtered tests:
-# ./scripts/build.sh ctest .*regex.*
-## or
-# ./scripts/build.sh gtest *wildcard*
-##
-# CC=cl CXX=cl VERBOSE=1 DEPLOY=1 BUILD_TYPE=Debug BUILD_TESTS="WNDX_ALGO_BUILD_TESTS" GENERATOR="Visual Studio 17 2022" ./scripts/build.sh c ct
 
 set -e
 
@@ -53,22 +40,63 @@ OPTIONS
 POSITIONAL
     c , clean  , --clean
     cc, cleaner, --cleaner
-    ctest, ct, ctp, ctr       (optional) arg - filter regex
-    gtest, gt                 (optional) arg - filter wildcard (:,*,?,-)
-                              ':'-separated patterns '*', '?' ('-' negative)
+    ctest, ct, ctp, ctr       (optional) arg - filter regex    (default: '.*')
+    gtest, gt                 (optional) arg - filter wildcard (default:  '*')
+                              special chars (:,*,?,-), ':' separator, '-' negative
 EXAMPLES
-$bname c ct .*
-$bname c gt *
+./scripts/$bname c ct
+./scripts/$bname c gt *
+./scripts/$bname
+# ====== build & run tests:
+./scripts/$bname ctest
+# ====== make clean build & run tests:
+./scripts/$bname clean ctest
+# ====== make cleaner build (for edge cases) & run tests:
+./scripts/$bname cleaner ctest
+# ====== build & run filtered tests:
+./scripts/$bname ctest .*regex.*
+# ====== or
+./scripts/$bname gtest *wildcard*
+# ======
+export CC=cl CXX=cl VERBOSE=1 DEPLOY=1 BUILD_TYPE=Debug GENERATOR='Visual Studio 17 2022'
+export BUILD_TESTS=WNDX_ALGO_BUILD_TESTS
+./scripts/$bname c ct
 "
 
+CMAKE="${CMAKE:-cmake}" # override cmake executable path via env var
 OLDIFS="$IFS"
 
-if [ ! -r ./CMakeLists.txt ]; then
-  printf "%s\n" "Current directory does not contain CMakeLists.txt, exit."
-  exit 1
-fi
-
 at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+
+printe() {
+  at_path printf || exit 63
+  if [ "$#" = 1 ]; then
+    printf "%s\n" "$*" 1>&2
+  else # shellcheck disable=SC2059 # intentional - re-split any amount of arguments.
+    printf "$@" 1>&2
+  fi
+}
+
+check_prerequisites() {
+  if [ ! -r ./CMakeLists.txt ]; then
+    printe "%s %s\n" "Current directory does not contain CMakeLists.txt!" "EXIT."
+    exit 64
+  fi
+  if ! at_path "$CMAKE"; then
+    printe "\$CMAKE='$CMAKE'- NOT VALID cmake executable"
+    printe "'cmake' - REQUIRED DEPENDENCY NOT FOUND AT \$PATH! EXIT."
+    exit 65
+  fi
+  IFS=" " # split arguments by space
+  deps="basename cut echo false getopt grep mkdir printf sed tr true uname"
+  for executable in $deps; do
+    if ! at_path "$executable"; then
+      printe "'$executable' - REQUIRED DEPENDENCY NOT FOUND AT \$PATH! EXIT."
+      exit 66
+    fi
+  done
+  IFS="$OLDIFS" # restore
+}
 
 get_prj_name() {
   if at_path rg; then
@@ -102,8 +130,8 @@ notify() {
 
 # NOTE: on win10 in MINGW64:/git-bash shell -- trailing args via -- '$*' - works; '$@' - NOT works;
 # ^ apparently in this environment - this function is still not always finds tests...
-# shellcheck disable=SC2048,SC2068,SC2086 # intentional - to re-split trailing arguments.
-run_ctest() { cmake -E time ctest --build-config "$BUILD_TYPE" --output-on-failure --test-dir "$_bdir/$TESTS_DIR" $* ;}
+# shellcheck disable=SC2048,SC2068,SC2086 # intentional - re-split trailing arguments.
+run_ctest() { "$CMAKE" -E time ctest --build-config "$BUILD_TYPE" --output-on-failure --test-dir "$_bdir/$TESTS_DIR" $* ;}
 
 SEP="=============================================================================="
 vsep() { printf "\n%b%.78s%b\n\n" "${2}" "[${1}]${SEP}" "${END}" ;}
@@ -173,7 +201,7 @@ trailing_args() {
     ;;
     *)
       targv=$(echo "$targv" | cut -d ' ' -f "${argn}-")
-      printf "unparsed trailing args[%d]=[%s]\nunsupported arg: [%s]\n" "$targc" "$targv" "$arg"
+      printe "unparsed trailing args[%d]=[%s]\nunsupported arg: [%s]\n" "$targc" "$targv" "$arg"
       exit 6
     ;;
     esac
@@ -181,7 +209,7 @@ trailing_args() {
   set +o noglob # restore - enable (wildcard expansion)
   IFS="$OLDIFS" # restore
   if true; then # debug
-    echo "CLEAN=$CLEAN CLEANER=$CLEANER RUN_TESTS_TYPE=$RUN_TESTS_TYPE TESTS_FILTER=$TESTS_FILTER"
+    printe "CLEAN=$CLEAN CLEANER=$CLEANER RUN_TESTS_TYPE=$RUN_TESTS_TYPE TESTS_FILTER=$TESTS_FILTER"
     exit 33
   fi
 }
@@ -262,23 +290,25 @@ get_opt() {
     esac
     shift
   done
-  pre_configure
 }
 
+## MAIN
+check_prerequisites
 get_opt "$@"
+pre_configure
 
 
 vsep "CONFIGURE" "${BLU}"
-# shellcheck disable=SC2086 # Intentional - re-split OPTIONS CONFIGURE
-cmake -E time \
-cmake -S "$_sdir" -B "$_bdir" -G "$GENERATOR" -Wdev -Werror=dev \
+# shellcheck disable=SC2086 # intentional - re-split OPTIONS CONFIGURE
+"$CMAKE" -E time \
+"$CMAKE" -S "$_sdir" -B "$_bdir" -G "$GENERATOR" -Wdev -Werror=dev \
 ${_fresh} ${_cmake_log_level} ${COPTS} \
 || { notify ERROR "CMAKE CONFIGURE ERROR" ; exit "$EC" ;}
 
 vsep "BUILD" "${CYN}"
-# shellcheck disable=SC2086 # Intentional - re-split OPTIONS BUILD
-cmake -E time \
-cmake --build "$_bdir" --config "${BUILD_TYPE}" ${_clean_first} ${_verbose} ${BOPTS} \
+# shellcheck disable=SC2086 # intentional - re-split OPTIONS BUILD
+"$CMAKE" -E time \
+"$CMAKE" --build "$_bdir" --config "${BUILD_TYPE}" ${_clean_first} ${_verbose} ${BOPTS} \
 || { notify ERROR "CMAKE BUILD ERROR" ; exit "$EC" ;}
 
 if [ "$RUN_TESTS" = 1 ]; then
@@ -297,7 +327,7 @@ if [ "$RUN_TESTS" = 1 ]; then
   gt|gtest)
     gtest_binary="./$_bdir/bin/tests_units"
     if [ ! -x "$gtest_binary" ]; then
-      printf "%s\n^ %s\n" "$gtest_binary" "File not found or not executable, exit."
+      printe "%s\n^ %s\n" "$gtest_binary" "File not found or not executable, exit."
       exit 5
     fi
     "$gtest_binary" --gtests_filter="$TESTS_FILTER"
@@ -307,7 +337,7 @@ fi
 
 if [ "$DEPLOY" = 1 ]; then
   vsep "DEPLOY" "${MAG}"
-  cmake --install "$_bdir" --config "${BUILD_TYPE}" --prefix "$_bdir/deploy"
+  "$CMAKE" --install "$_bdir" --config "${BUILD_TYPE}" --prefix "$_bdir/deploy"
 fi
 
 vsep   "COMPLETED" "${GRN}"
