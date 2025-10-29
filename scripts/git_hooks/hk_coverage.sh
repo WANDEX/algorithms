87ebb114f74462a25a39433f4a0fa265d9c37d6e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-10-30 01:44:00 +0300

    [hk_coverage.sh] use of --get_project_name_upper

--- scripts/git_hooks/hk_coverage.sh
+++ scripts/git_hooks/hk_coverage.sh
@@ -7,15 +7,18 @@ export CC=gcc   CXX=g++
 
 export BUILD_TYPE=Debug
 
+build_script="./scripts/wndx_cmake_build.sh"
+PRJ_NAME_UPPER=$("$build_script" --get_project_name_upper)
+
 ## env variable provided at cmake configure step inside build.sh
 export COPTS=\
-"-D WNDX_ALGO_BUILD_TESTS=ON
- -D WNDX_ALGO_COVERAGE_ENABLE=ON
- -D WNDX_ALGO_COVERAGE_CLEAN=ON
+"-D ${PRJ_NAME_UPPER}_BUILD_TESTS=ON
+ -D ${PRJ_NAME_UPPER}_COVERAGE_ENABLE=ON
+ -D ${PRJ_NAME_UPPER}_COVERAGE_CLEAN=ON
 "
 
 echo "${BLU}build & run tests for the fresh coverage report...${END}"
-./scripts/wndx_cmake_build.sh ctp
+"$build_script" ctp
 echo "${BLU}generate lcov coverage report...${END}"
 ./scripts/lcov.sh
 echo "${BLU}update coverage in README.md...${END}"
